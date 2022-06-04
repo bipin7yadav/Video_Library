785fd3addf4bebed47de5f33b5d8d9886c89759f
Bipin Yadav <bipinyadav9769@gmail.com>
2022-06-04 16:35:15 +0530

    Like , History , WatchLater
    - Added history , WatchLater and like page with functionality
    
    - Added Toast

--- src/pages/likedVideos/LikedVideos.js
+++ src/pages/likedVideos/LikedVideos.js
@@ -3,10 +3,19 @@ import "../../global.css"
 import { Sidebar } from '../../components';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { deleteLike } from '../videoSlice/VideoSlice';
+import { toast } from 'react-toastify';
 
 
 const LikedVideos = () => {
-    const dispatch =useDispatch()
+    const likes = useSelector((state) => state.video.likedVideos)
+    const dispatch = useDispatch()
+
+    function likeHandler(a) {
+        toast.success("removed from liked videos ")
+        dispatch(deleteLike(a))
+    }
+
     return (
         <>
             <div className='flex-rowns margin'>
@@ -16,6 +25,36 @@ const LikedVideos = () => {
                     </div>
                 </div>
                 <div className='mainContent'>
+
+                    <div className='items gap'>
+                        {
+                            likes.length > 0 ?
+                                likes.map((a) => {
+                                    return (
+                                        <>
+                                            <div className='card gap'>
+                                                <div >
+                                                    <Link style={{ textDecoration: "none" }} to={`/video/${a.src}`}><img onClick={() => { play(a) }} className='thumbnail' src={`http://img.youtube.com/vi/${a.src}/mqdefault.jpg`} /></Link>
+                                                </div>
+                                                <div className='flex-row'>
+                                                    <div className='data'>
+                                                        <div>{a.title}</div>
+                                                        <div className='flex-row view'>
+                                                            <div className='creator'>{a.creator}</div>
+                                                            <div>{a.view} views</div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <div><span class="material-icons delete" onClick={() => likeHandler(a)}>delete</span></div>
+                                            </div>
+
+                                        </>
+                                    )
+                                })
+                                : <h2 className='msg'>No Liked Videos</h2>
+                        }
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/screenPlay/screenPlay.js
+++ src/pages/screenPlay/screenPlay.js
@@ -4,7 +4,10 @@ import { useParams } from 'react-router-dom';;
 import { Sidebar } from '../../components';
 import "./screenPlay.css"
 import { useSelector, useDispatch } from 'react-redux';
+import { addLike, addWatch ,addHistory , deleteLike ,deleteWatchLater} from '../videoSlice/VideoSlice';
 import {v4 as uuid} from "uuid"
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ScreenPlay = () => {
 
@@ -17,6 +20,32 @@ const ScreenPlay = () => {
 
     const post = data.find((a) => a.src === videoId)
 
+    const [Like,setLike]=useState(true)
+    const [watch,setWatch]=useState(true)
+    const [play,setPlay]=useState(true)
+
+    function like(post){
+        setLike(!Like)
+        if(Like){
+            toast.success("Video Liked")
+            dispatch(addLike(post))
+        }else{
+            toast.warning("video unliked")
+            dispatch(deleteLike(post))
+        }
+
+    }
+
+    function watchLater(post){
+        setWatch(!watch)
+        if(watch){
+            toast.success("added to watchLater")
+            dispatch(addWatch(post))
+        }else{
+            toast.warning("removed from watchLater")
+            dispatch(deleteWatchLater(post))
+        }
+    }
         
     
     return (
@@ -36,6 +65,7 @@ const ScreenPlay = () => {
                         width="100%"
                         height="100%"
                         controls={true}
+                        onPlay={()=>dispatch(addHistory(post))}
                     />
 
                 </div>
@@ -46,13 +76,13 @@ const ScreenPlay = () => {
                     <div>{post.description}</div>
                     <div className='simple-flex-row btn-gap'>
                         <div>
-                            <span class="material-icons icon"  >playlist_add_circle</span>
+                            <span className="material-icons icon" style={{color:play?"black":"blue"}}  >playlist_add_circle</span>
                         </div>
                         <div>
-                            <span class="material-icons icon"  >thumb_up</span>
+                            <span className="material-icons icon" style={{color:Like?"black":"blue"}} onClick={() => like(post)}>thumb_up</span>
                         </div>
                         <div>
-                            <span class="material-icons icon"  >watch_later</span>
+                            <span className="material-icons icon" style={{color:watch?"black":"blue"}} onClick={() => watchLater(post)}>watch_later</span>
                         </div>
                     </div>
 
