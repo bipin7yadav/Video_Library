a247c6723d4b50c133e626beb057af6db64e76f2
Bipin Yadav <86704062+bipin7yadav@users.noreply.github.com>
2022-06-08 23:48:14 +0530

    Update screenPlay.js

--- src/pages/screenPlay/screenPlay.js
+++ src/pages/screenPlay/screenPlay.js
@@ -100,7 +100,7 @@ const ScreenPlay = () => {
 
     return (
         <>
-            <div className='modals' style={{ visibility: modal ?  "visible": "hidden" }} >
+            <div className='modals' style={{ visibility: modal ? "hidden" :"visible" }} >
                 <div className='contents'>
                     <div>Add PlayList</div>
                     <div>
