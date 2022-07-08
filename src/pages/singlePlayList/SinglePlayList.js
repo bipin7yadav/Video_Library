7d695b3bc4adfbdd2d822a33f45cbeeec176843a
Bipin Yadav <bipinyadav9769@gmail.com>
2022-07-08 10:11:30 +0530

    Responsiveness

--- src/pages/singlePlayList/SinglePlayList.js
+++ src/pages/singlePlayList/SinglePlayList.js
@@ -12,20 +12,19 @@ const SinglePlayList = () => {
 
     const dispatch = useDispatch()
 
-    const {playlist}=useSelector((state)=>state.features)
-    const [dependency,setDependency] = useState(false);
+    const { playlist } = useSelector((state) => state.features)
+    const [dependency, setDependency] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(playListGet())
-    },[dependency])
+    }, [dependency])
 
 
-    let mapPlaylist ={videos:[]}
-    if (playlist !== undefined){
-         mapPlaylist= playlist.find((elem)=>elem._id==playlistId)
+    let mapPlaylist = { videos: [] }
+    if (playlist !== undefined) {
+        mapPlaylist = playlist.find((elem) => elem._id == playlistId)
 
     }
-     console.log(playlistId,"id");
 
     return (
         <div className="flex-rowns margin">
@@ -34,34 +33,37 @@ const SinglePlayList = () => {
                     <Sidebar />
                 </div>
             </div>
-            <div className="items">
-                {
-                    mapPlaylist !== undefined  ?
-                    mapPlaylist.videos.map((item) => {
-                            return (
-                                <div key={item.id}>
-                                    <div className='card gap'>
-                                        <div >
-                                            <Link style={{ textDecoration: "none" }} to={`/video/${item.src}`}><img  className='thumbnail' src={`http://img.youtube.com/vi/${item.src}/mqdefault.jpg`} /></Link>
-                                        </div>
-                                        <div className='flex-row'>
-                                            <div className='data'>
-                                                <div>{item.title}</div>
-                                                <div className='flex-row view'>
-                                                    <div className='creator'>{item.creator}</div>
-                                                    <div>{item.view} views</div>
-                                                </div>
+            <div className="mainContent">
+                <div className="items">
+                    {
+                        mapPlaylist !== undefined ?
+                            mapPlaylist.videos.map((item) => {
+                                return (
+                                    <div key={item.id}>
+                                        <div className='card gap'>
+                                            <div >
+                                                <Link style={{ textDecoration: "none" }} to={`/video/${item.src}`}><img className='thumbnail' src={`http://img.youtube.com/vi/${item.src}/mqdefault.jpg`} /></Link>
                                             </div>
-                                            <div><span className="material-icons delete" onClick={() =>{
-                                                setDependency(!dependency)
-                                                dispatch(deletePlaylistVideo({mapPlaylist,item}))}} >delete</span></div>
+                                            <div className='flex-row'>
+                                                <div className='data'>
+                                                    <div>{item.title}</div>
+                                                    <div className='flex-row view'>
+                                                        <div className='creator'>{item.creator}</div>
+                                                        <div>{item.view} views</div>
+                                                    </div>
+                                                </div>
+                                                <div><span className="material-icons delete" onClick={() => {
+                                                    setDependency(!dependency)
+                                                    dispatch(deletePlaylistVideo({ mapPlaylist, item }))
+                                                }} >delete</span></div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })
-                        :<h2 className="msg">Playlist is empty !!</h2>
-                }
+                                )
+                            })
+                            : <h2 className="msg">Playlist is empty !!</h2>
+                    }
+                </div>
             </div>
         </div>
     );
